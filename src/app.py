178c706f1802a19319dca4c65946d6cb62f4fe7f
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-09-14 11:29:23 +0000

    arreglos en los modelos

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, UserImage, Service, Comment, Reservation, SavedService, Familiar
 #from models import Person
 
 app = Flask(__name__)
@@ -49,3 +49,18 @@ def handle_hello():
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    all_users = User.query.all()
+    map_users = list(map(lambda item: item.serialize(), all_users ))
+
+    if all_users == []:
+        return {"msg":"No hay usuarios creados"}, 404
+
+    response_body = {
+        "msg": "Usuarios creados: ",
+        "Users" : map_users
+    }
+
+    return jsonify(response_body), 200
--- src/models.py
+++ src/models.py
@@ -28,9 +28,36 @@ class User(db.Model):
     image_url = Column(String(255), nullable=True)
     ciudad = Column(String(50), nullable=True)
     available_times = Column(JSON, nullable=True)  # Nuevo campo para horarios disponibles
-    # reservations = relationship('Reservation', backref='client', lazy=True)
-    # services = relationship('Service', backref='user', lazy=True)
-    # accepted_reservations = relationship('Reservation', primaryjoin="and_(User.id==Reservation.client_id, Reservation.accept==True)", lazy=True)
+    reservations = relationship('Reservation', backref='client', lazy=True)
+    services = relationship('Service', backref='user', lazy=True)
+    accepted_reservations = relationship('Reservation', primaryjoin="and_(User.id==Reservation.client_id, Reservation.accept==True)", lazy=True)
+    userImage = db.relationship('UserImage', backref='user', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "password": self.password,
+            "is_client": self.is_client,
+            "sexo": self.sexo,
+            "altura": self.altura,
+            "nombre": self.nombre,
+            "peso": self.peso,
+            "edad": self.edad,
+            "fecha_de_nacimiento": self.fecha_de_nacimiento,
+            "alergias_alimentarias": self.alergias_alimentarias,
+            "medicamentos_actuales": self.medicamentos_actuales,
+            "vacunas": self.vacunas,
+            "historial_familiar": self.historial_familiar,
+            "numero_salud": self.numero_salud,
+            "image_url": self.image_url,
+            "ciudad": self.ciudad,
+            "available_times": self.available_times,
+        }
 
 class UserImage(db.Model):
     __tablename__ = 'user_image'
@@ -43,65 +70,65 @@ class UserImage(db.Model):
 class Service(db.Model):
     __tablename__ = 'service'
     id = db.Column(db.Integer, primary_key=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     address = db.Column(db.String(255), nullable=False)
     # occupations = db.Column(db.JSON, nullable=False, default=[])  # Cambiado a lista vacía
     # type = db.Column(db.Enum('telemedicina', 'domicilio', 'ambos'), nullable=False)
     # prices = db.Column(db.JSON, nullable=True, default={})  # Agregar este campo
-    # reservations = db.relationship('Reservation', backref='service', lazy=True)
+    reservations = db.relationship('Reservation', backref='service', lazy=True)
 
 
 class Comment(db.Model):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    # service_id = Column(Integer, ForeignKey('service.id'), nullable=False)
+    service_id = Column(Integer, ForeignKey('service.id'), nullable=False)
     text = Column(String(255), nullable=False)
-    # service = relationship('Service', backref=db.backref('comments', lazy=True))
+    service = relationship('Service', backref=db.backref('comments', lazy=True))
 
 class Reservation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # client_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    # service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
-    # reservation_date = db.Column(db.DateTime, nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    reservation_date = db.Column(db.DateTime, nullable=False)
     type = db.Column(db.String(50), nullable=False)
-    # patient_name = db.Column(db.String(100), nullable=False)
-    # address = db.Column(db.String(200), nullable=False)
-    # time_slot = db.Column(db.String(50), nullable=False)
-    # comment = db.Column(db.String(500))
-    # accept = db.Column(db.Boolean, default=False)
+    patient_name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    time_slot = db.Column(db.String(50), nullable=False)
+    comment = db.Column(db.String(500))
+    accept = db.Column(db.Boolean, default=False)
     
     def to_dict(self):
         return {
             'id': self.id,
-            # 'client_id': self.client_id,
-            # 'service_id': self.service_id,
+            'client_id': self.client_id,
+            'service_id': self.service_id,
             # 'reservation_date': self.reservation_date.isoformat(),
             'type': self.type,
-            # 'patient_name': self.patient_name,
-            # 'address': self.address,
-            # 'time_slot': self.time_slot,
-            # 'comment': self.comment,
-            # 'accept': self.accept
+            'patient_name': self.patient_name,
+            'address': self.address,
+            'time_slot': self.time_slot,
+            'comment': self.comment,
+            'accept': self.accept
         }
 
 class SavedService(db.Model):
     __tablename__ = 'saved_service'
     id = Column(Integer, primary_key=True)
-    # user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    # service_id = Column(Integer, ForeignKey('service.id'), nullable=False)
-    # saved_date = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    service_id = Column(Integer, ForeignKey('service.id'), nullable=False)
+    saved_date = Column(DateTime, default=datetime.utcnow)
 
-    # user = relationship('User', backref='saved_services', lazy=True)
-    # service = relationship('Service', backref='saved_by_users', lazy=True)
+    user = relationship('User', backref='saved_services', lazy=True)
+    service = relationship('Service', backref='saved_by_users', lazy=True)
 
 class Familiar(db.Model):
     __tablename__ = 'familiar'
     id = Column(Integer, primary_key=True)
     nombre = Column(String(80), nullable=False)
     parentesco = Column(String(50), nullable=False)
-    # user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    # user = relationship('User', backref='familiares')
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship('User', backref='familiares')
 
     def __repr__(self):
         return f'<Familiar(nombre={self.nombre}, parentesco={self.parentesco})>'
